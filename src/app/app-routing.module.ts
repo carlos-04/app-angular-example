dbefcf524963ea387ff5131a63895fb5305b0f96
Carlos J Montero <carlos_j_montero@claro.com.do>
2022-04-06 19:41:03 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NosotrosComponent } from './components/pages/nosotros/nosotros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'nosotros', component:NosotrosComponent},
+  {path:'', pathMatch:'full', redirectTo:'home'},
+  {path:'**', pathMatch:'full', redirectTo:'home'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { NosotrosComponent } from './components/pages/nosotros/nosotros.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NosotrosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
